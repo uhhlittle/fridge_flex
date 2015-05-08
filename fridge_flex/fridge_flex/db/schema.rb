0dfe8ba5509ffe2cefce419b3aeba5680ad39a43
uhhlittle <allysonjlittle@gmail.com>
2015-05-08 17:08:23 -0400

    profile model added, migrated foreign key to devise user model

--- fridge_flex/fridge_flex/db/schema.rb
+++ fridge_flex/fridge_flex/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508210121) do
+ActiveRecord::Schema.define(version: 20150508210701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
