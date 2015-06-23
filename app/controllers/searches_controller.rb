17f51a433f157b84634d19aed14f35b13f2e3e11
uhhlittle <allysonjlittle@gmail.com>
2015-06-23 10:11:57 -0400

    SEARCH FUNCTION WORKING WOOHOOgit add .  api keys still exposed. hide that ish

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,13 @@
 class SearchesController < ApplicationController
   def get_search_result
     @fridge = current_user.profile.fridge
-    # @ingredients = @fridge.ingredients
-    # @ingredient_string = [].join("+")
-    # @ingredients.each do |ingredient|
-    #   @ingredient_string << ingredient.name
-    # end
+    @ingredients = @fridge.ingredients
+    @ingredient_string = [].join("+")
+    @ingredients.each do |ingredient|
+      @ingredient_string << ingredient.name
+    end
     # @search_results = F2F.new(params[:q]).perform.results
-    @search_results = FoodApi.new("black+beans").perform.results
+    @search_results = FoodApi.new(@ingredient_string).perform.results
     render json: @search_results
   end
 end
